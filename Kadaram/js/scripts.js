05143904f49432f836978f4adb365e3cba31b85a
W. Pei min <91327383+Whitecrow1208@users.noreply.github.com>
2022-10-08 01:59:44 +0800

    Add files via upload

---
+++ Kadaram/js/scripts.js
@@ -0,0 +1,23 @@
+
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
